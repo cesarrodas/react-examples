c7bfc6e41635b977da3745dff7c0777271dca03f
Cesar Rodas <rodascesar03@yahoo.com>
2017-08-11 10:19:33 -0500

    updated dependencies and started using router v4

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Hello } from './heyfile';
 require('../styles/styles.scss');
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+const Hey = () => (<div><h1>HEYLLO</h1></div>);
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Hello} />
+      <Route path="/about" component={Hey} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,23 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/js/main.js',
   output: {
-    path: 'build',
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   devServer: {
     inline: true,
     contentBase: './build',
-    port: 3000
+    port: 3000,
+    historyApiFallback: true
   },
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react', 'stage-0']
         }
